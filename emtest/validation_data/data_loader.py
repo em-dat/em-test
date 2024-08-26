d295fde8bdcbfa8d35808ba88f4c62cc6f7b830f
Damien Delforge <damien.delforge@uclouvain.be>
2024-08-26 14:52:30 +0200

    Add validation data file for GAUL administrative codes
    This commit introduces new data files, `gaul_adm1_code.txt` `gaul_adm2_code.txt`, containing administrative codes for validation purposes.

--- emtest/validation_data/data_loader.py
+++ emtest/validation_data/data_loader.py
@@ -1,14 +1,25 @@
 from pathlib import Path
+from typing import Literal
 
 import pandas as pd
 import toml
 
 CLASSIFICATION_FILE = Path(
-    f"{Path(__file__).parent}/classification_tree.toml")
+    f"{Path(__file__).parent}/classification_tree.toml"
+)
 
 # accessed 24.07.02
 AREAS_FILE = Path(
-    f"{Path(__file__).parent}/UNSD_M49_standards.csv")
+    f"{Path(__file__).parent}/UNSD_M49_standards.csv"
+)
+
+GAUL_ADM1_FILE = Path(
+    f"{Path(__file__).parent}/gaul_adm1_code.txt"
+)
+
+GAUL_ADM2_FILE = Path(
+    f"{Path(__file__).parent}/gaul_adm2_code.txt"
+)
 
 
 def load_classification(file: Path = CLASSIFICATION_FILE):
@@ -40,6 +51,32 @@ def load_classification(file: Path = CLASSIFICATION_FILE):
     rows = data['rows']
     df = pd.DataFrame(rows)
     return df
+
+
+def load_GAUL_code(level: Literal[1, 2]) -> list:
+    """Load and return a list of GAUL Admin. 1 or Admin. 2 codes
+
+    Parameters
+    ----------
+    level : int
+        Administrative level. Either 1 or 2
+
+    Example
+    -------
+
+    >>> adm_code1 = load_GAUL_code(1)
+    >>> adm_code1[:5]
+    ['1232', '2112', '2119', '1357', '1066']
+
+    """
+    if level == 1:
+        file = GAUL_ADM1_FILE
+    elif level == 2:
+        file = GAUL_ADM2_FILE
+    with open(file, 'r', encoding='utf-8') as file:
+        lines = [line.strip() for line in file]
+
+    return lines
 
 
 def load_UNSD_areas(file: Path = AREAS_FILE):
